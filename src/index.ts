e26be9263d8aff72778a04bde1707a3410e81e95
Leon San José Larsson <leonlarsson8@gmail.com>
2024-03-25 20:18:14 +0100

    implement cache

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,17 @@
 import { Hono } from "hono";
+import { cache } from "hono/cache";
 import getLeaderboard from "./handlers/v1/getLeaderboard";
 
 const app = new Hono();
+
+// Cache for 10 minutes
+app.get(
+  "/v1/leaderboard/*",
+  cache({
+    cacheName: "v1-leaderboard",
+    cacheControl: "public, max-age=600",
+  })
+);
 
 app.get("/v1/leaderboard/:leaderboardVersion?/:platform?", getLeaderboard);
 
