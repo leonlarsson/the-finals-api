79a79c352e4d5cbd6329abe2da882d17737afabd
Leon San José Larsson <54620188+leonlarsson@users.noreply.github.com>
2024-06-03 16:40:44 +0200

    add GET /210event to get the new community event data

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import { cors } from "hono/cors";
 import { cache } from "hono/cache";
 import getLeaderboard from "./handlers/v1/getLeaderboard";
 import proxyUrl from "./handlers/proxy/proxyUrl";
+import get210Event from "./handlers/get210Event";
 
 const app = new Hono();
 
@@ -18,7 +19,18 @@ app.get(
   })
 );
 
+// Cache for 30 minutes
+app.get(
+  "/210event",
+  cache({
+    cacheName: "210event",
+    cacheControl: "public, max-age=1800",
+  })
+);
+
+// Routes
 app.get("/v1/leaderboard/:leaderboardVersion?/:platform?", getLeaderboard);
 app.get("/proxy", proxyUrl);
+app.get("/210event", get210Event);
 
 export default app;
