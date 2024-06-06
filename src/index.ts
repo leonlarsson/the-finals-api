c0516053dd759ceb742cac4ad34ac53041287552
Leon San José Larsson <54620188+leonlarsson@users.noreply.github.com>
2024-06-06 14:44:35 +0200

    create cache middleware wrapper

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import { Hono } from "hono";
 import { cors } from "hono/cors";
-import { cache } from "hono/cache";
+import { cache } from "./middleware/cache";
 import getLeaderboard from "./handlers/v1/getLeaderboard";
 import proxyUrl from "./handlers/proxy/proxyUrl";
 import get210Event from "./handlers/get210Event";
@@ -10,27 +10,13 @@ const app = new Hono();
 // Enable CORS for all routes
 app.use("*", cors());
 
-// Cache for 10 minutes
-app.get(
-  "/v1/leaderboard/*",
-  cache({
-    cacheName: "v1-leaderboard",
-    cacheControl: "public, max-age=600",
-  })
-);
-
-// Cache for 30 minutes
-app.get(
-  "/210event",
-  cache({
-    cacheName: "210event",
-    cacheControl: "public, max-age=1800",
-  })
-);
-
 // Routes
-app.get("/v1/leaderboard/:leaderboardVersion?/:platform?", getLeaderboard);
+app.get(
+  "/v1/leaderboard/:leaderboardVersion?/:platform?",
+  cache("v1-leaderboard", 10),
+  getLeaderboard
+);
+app.get("/210event", cache("210event", 30), get210Event);
 app.get("/proxy", proxyUrl);
-app.get("/210event", get210Event);
 
 export default app;
---
+++ src/middleware/cache.ts
@@ -0,0 +1,7 @@
+import { cache as honoCache } from "hono/cache";
+
+export const cache = (name: string, minutes: number) =>
+  honoCache({
+    cacheName: name,
+    cacheControl: `public, max-age=${minutes * 60}`,
+  });
