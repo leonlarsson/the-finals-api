6a45116f085de2a8d33ef49f55bf0beed5f0c9c9
Leon San José Larsson <54620188+leonlarsson@users.noreply.github.com>
2024-08-30 17:38:46 +0200

    temp fix s3worldtour

--- src/transformers/season3WorldTour.ts
+++ src/transformers/season3WorldTour.ts
@@ -2,20 +2,34 @@ import { z } from "zod";
 
 export const season3WorldTourSchema = z
   .object({
-    r: z.number(),
-    name: z.string(),
-    p: z.number(),
-    steam: z.string(),
-    xbox: z.string(),
-    psn: z.string(),
+    // 2024-08-30: Embark either messed up or dislike community tools. All keys are now numbers instead of somewhat descriptive strings. Empty platform names are now 0 instead of empty strings.
+    // r: z.number(),
+    // name: z.string(),
+    // p: z.number(),
+    // steam: z.string(),
+    // xbox: z.string(),
+    // psn: z.string(),
+    1: z.number(),
+    3: z.string(),
+    5: z.number(),
+    6: z.union([z.string(), z.number()]),
+    7: z.union([z.string(), z.number()]),
+    8: z.union([z.string(), z.number()]),
   })
   .transform(data => ({
-    rank: data.r,
-    name: data.name,
-    steamName: data.steam,
-    xboxName: data.xbox,
-    psnName: data.psn,
-    cashouts: data.p,
+    // 2024-08-30: Embark either messed up or dislike community tools
+    // rank: data.r,
+    // name: data.name,
+    // steamName: data.steam,
+    // xboxName: data.xbox,
+    // psnName: data.psn,
+    // cashouts: data.p,
+    rank: data[1],
+    name: data[3],
+    steamName: data[6] === 0 ? "" : data[6],
+    xboxName: data[7] === 0 ? "" : data[7],
+    psnName: data[8] === 0 ? "" : data[8],
+    cashouts: data[5],
   }))
   .array();
 
