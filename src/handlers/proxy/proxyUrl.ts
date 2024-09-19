d7613d6d4b031f39d1a258f9722bcbf867e5f58d
Leon San José Larsson <54620188+leonlarsson@users.noreply.github.com>
2024-09-19 13:48:39 +0200

    update proxy

--- src/handlers/proxy/proxyUrl.ts
+++ src/handlers/proxy/proxyUrl.ts
@@ -3,9 +3,5 @@ import { Context } from "hono";
 export default async (c: Context) => {
   const { url } = c.req.query();
   if (!url) return c.json({ error: "No URL provided." }, 400);
-
-  const response = await fetch(url);
-  const data = await response.json();
-
-  return c.json(data as any);
+  return fetch(url);
 };
