9e811a724626c8f55826010026322d5726cef1c8
Leon San José Larsson <54620188+leonlarsson@users.noreply.github.com>
2024-10-19 17:22:56 +0200

    add some property examples

--- src/schemas/responseMeta.ts
+++ src/schemas/responseMeta.ts
@@ -2,9 +2,11 @@ import { z } from "zod";
 
 export const responseMetaSchema = z
   .object({
-    leaderboardVersion: z.string().openapi({ description: "The leaderboard version selected." }),
-    leaderboardPlatform: z.string().openapi({ description: "The leaderboard platform selected." }),
-    nameFilter: z.string().optional().openapi({ description: "The chosen name filter." }),
+    leaderboardVersion: z.string().openapi({ description: "The leaderboard version selected.", example: "s4" }),
+    leaderboardPlatform: z
+      .string()
+      .openapi({ description: "The leaderboard platform selected.", example: "crossplay" }),
+    nameFilter: z.string().optional().openapi({ description: "The chosen name filter.", example: "ttv" }),
     returnCountOnly: z
       .boolean()
       .openapi({ description: "Whether the response only contains the count of the leaderboard." }),
