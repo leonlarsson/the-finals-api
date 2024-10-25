467f737a4c1c05e75d675b088646387e8a2fcdb0
Leon San José Larsson <54620188+leonlarsson@users.noreply.github.com>
2024-10-25 12:50:56 +0200

    add proper examples to communityEventProgressSchema

--- src/schemas/communityEvents/communityEventProgress.ts
+++ src/schemas/communityEvents/communityEventProgress.ts
@@ -2,8 +2,10 @@ import { z } from "zod";
 
 export const communityEventProgressSchema = z
   .object({
-    currentProgress: z.number().openapi({ description: "The current progress of the community event." }),
-    goal: z.number().openapi({ description: "The goal of the community event." }),
+    currentProgress: z
+      .number()
+      .openapi({ description: "The current progress of the community event.", example: 11_333_777 }),
+    goal: z.number().openapi({ description: "The goal of the community event.", example: 30_000_000 }),
   })
   .openapi("CommunityEventProgress", {
     title: "Community Event Progress",
