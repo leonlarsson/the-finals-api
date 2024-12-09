0cae0b1d97e57e5a497f3da53e81197f92ccbea1
Leon San José Larsson <54620188+leonlarsson@users.noreply.github.com>
2024-12-09 15:18:46 +0100

    ce48: use static data

--- src/apis/communityEvents.ts
+++ src/apis/communityEvents.ts
@@ -1,8 +1,6 @@
 import { ce44ResponseSchema, ce44Schema } from "../schemas/communityEvents/ce44";
 import { ce48ResponseSchema, ce48Schema } from "../schemas/communityEvents/ce48";
 import type { BaseAPIRoute } from "../types";
-import { fetchWithKVFallback } from "../utils/fetchWithKVFallback";
-import fetchCe48Data from "../utils/fetchers/fetchCe48Data";
 import { getJsonFromKV } from "../utils/kv";
 
 // 20_160 minutes is 14 days
@@ -32,10 +30,10 @@ export const communityEventApiRoutes: BaseAPIRoute[] = [
       description:
         "Get leaderboard data from Community event 4.8.<br/>**Goal: Steal 600,000 cashouts in the final moments.**",
       tags: ["Community Events"],
+      cacheMinutes: oldLeaderboardCacheMinutes,
     },
-    includeInBackup: true,
     fetchData: async ({ kv }) => {
-      return await fetchWithKVFallback(fetchCe48Data, kv, "backup_event_ce48");
+      return await getJsonFromKV(kv, "data_event_ce48");
     },
     zodSchema: ce48Schema,
     zodSchemaOpenApi: ce48ResponseSchema,
